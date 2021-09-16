7ba721f839240908dbc1bdde960dfbafc35569b9
Ole Streicher <ole@aip.de>
2021-09-16 17:45:30 +0200

    Add copyright

--- doc/conf.py
+++ doc/conf.py
@@ -5,6 +5,7 @@ import sys
 project = 'IRAF'
 release = '2.16.2'
 version = release
+copyright = '1986 Association of Universities for Research in Astronomy Inc.'
 
 master_doc = 'index'
 extensions = [
